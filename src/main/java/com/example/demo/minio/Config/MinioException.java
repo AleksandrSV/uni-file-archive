1435f04ac94ae5c2b9e116758ccbdad854585701
IliaDuhov <dukhov03@mail.ru>
2023-07-14 20:55:11 +0400

    ReWork

--- src/main/java/com/example/demo/minio/Config/MinioException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.minio.Config;
-
-public class MinioException extends Exception {
-    public MinioException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/example/demo/minio/Config/MinioFetchException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.minio.Config;
-
-public class MinioFetchException extends RuntimeException{
-    public MinioFetchException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
